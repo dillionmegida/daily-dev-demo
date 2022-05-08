16c5b3248e29299a71da0f4285411e045d02cb9c
Dillion Megida <dillionmegida@gmail.com>
2022-05-08 21:28:24 +0200

    feat: init directory for daily-dev

---
+++ daily-dev/src/constants.js
@@ -0,0 +1,114 @@
+export const DEMO_DATA = [
+  {
+    name: "Deliver data services that accelerate your business Run your stateful workloads at scale. Sign up now",
+    image:
+      "https://media.ethicalads.io/media/images/2022/04/Deliver_data_services_kHsQlS3.png",
+    sponsored: {
+      by: "EthicalAds",
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/e258fd9dff884055b7b59df7fd1d7bd7",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "Typescript Shorts -Module Augmentation",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/c35521f5554994ce9dbde49860910020",
+    reactions: {
+      likes: 0,
+      comments: 0,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 2,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 1,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 1,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 1,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 1,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 1,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+  {
+    logo: "https://res.cloudinary.com/daily-now/image/upload/t_logo,f_auto/v1/logos/tc",
+    date: "May 05, 2022",
+    readTime: "5m",
+    name: "GitHub will require all users who contribute code to enable two-factor authentication by the end of 2023 – TechCrunch",
+    image:
+      "https://res.cloudinary.com/daily-now/image/upload/f_auto,q_auto/v1/posts/a909c019ea55e85fa320fabb0941c96b",
+    reactions: {
+      likes: 1,
+      comments: 1,
+      bookmarks: 0,
+    },
+  },
+]
